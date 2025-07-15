5615340b47658b506bf9f1030cc292839d130a8f
Barbara Inostroza~ <antonia.inostroza711@gmail.com~>
2025-07-15 00:02:00 -0400

    Cambios mios 15-07

--- backend/src/controllers/votation.controller.js
+++ backend/src/controllers/votation.controller.js
@@ -3,18 +3,22 @@ import { Vote }from "../entity/vote.entity.js";
 import { AppDataSource } from "../config/configDb.js";
 import { getResultadosVotacion } from "../services/votation.service.js";
 import { ActiveParticipantsEntity } from "../entity/activeParticipants.entity.js";
+import { votationValidation } from "../validations/votation.validation.js";
 
 
 export async function postularCandidatos(req, res) {
     try {
-        const { rut, nombre, apellido, cargo } = req.body;
-
-        if (!rut || !nombre || !apellido || !cargo) {
-            return res.status(400).json({ message: "Faltan datos para la postulación." });
-        }
+        const { body } = req;
+        
+            const { error } = votationValidation.validate(body);
+        
+            if (error) {
+                return res.status(400).json({message: "Error de validación", error: error.message});
+            }
 
     const votationRepo = AppDataSource.getRepository(ActiveParticipantsEntity);
 
+    const { rut, nombre, apellido, cargo } = req.body;
         // Verificar si ya está postulado al mismo cargo
         const candidatoExistente = await votationRepo.findOneBy({ rut, cargo });
         if (candidatoExistente) {
@@ -37,7 +41,7 @@ export async function postularCandidatos(req, res) {
 export async function mostrarCandidatos(req, res) {
 try {
     const activeRepo = AppDataSource.getRepository(ActiveParticipantsEntity);
-    const candidatos = await activeRepo.find({relations: ["user"] });
+    const candidatos = await activeRepo.find({relations: ["votes"] });
     
     if (candidatos.length === 0) {
     return res.status(400).json({ message: "No hay candidatos postulados." });
@@ -52,16 +56,15 @@ try {
 
     return res.status(200).json({ message: "Candidatos Encontrados.", candidatos });
 } catch (error) {
-    console.error("Error al mostrar candidatos:", error);
     return res.status(500).json({ message: "Error interno del servidor" });
 }
 }
 
 export async function emitirVoto(req, res) {
   try {
-    const { rut_votante, id_candidato } = req.body;
+    const { rut_votante, nombre_candidato, apellido_candidato, cargo} = req.body;
 
-    if (!rut_votante || !id_candidato) {
+    if (!rut_votante || !nombre_candidato || !apellido_candidato || !cargo) {
       return res.status(400).json({ message: "Verifica los datos para emitir el voto." });
     }
 
