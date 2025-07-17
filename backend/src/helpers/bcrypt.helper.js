23ea390ae1b606a985ff7d4122f1a323d5add221
raik999 <raimundo.koch2301@alumnos.ubiobio.cl>
2025-07-16 22:17:07 -0400

    a

--- backend/src/helpers/bcrypt.helper.js
+++ backend/src/helpers/bcrypt.helper.js
@@ -1,11 +1,24 @@
 "use strict";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
+// Función helper para encriptar y contraseñas
 export async function encryptPassword(password) {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
+  try {
+    // Generar un salt y encriptar la contraseña
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  } catch (error) {
+    console.error("Error en bcrypt.helper.js -> encryptPassword(): ", error);
+    throw new Error("Error al encriptar la contraseña");
+  }
 }
 
+// Función helper para comparar contraseñas
 export async function comparePassword(password, receivedPassword) {
-  return await bcrypt.compare(password, receivedPassword);
+  try {
+    return await bcrypt.compare(password, receivedPassword);
+  } catch (error) {
+    console.error("Error en bcrypt.helper.js -> comparePassword(): ", error);
+    throw new Error("Error al comparar las contraseñas");
+  }
 }
