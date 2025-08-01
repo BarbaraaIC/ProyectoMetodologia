4d58f2f3cbb9d0dd433606ac98446ded09204f59
Francisco <francisco.matamala2301@alumnos.ubiobio.cl>
2025-08-01 01:02:00 -0400

    arreglaod mari

--- frontend/src/hooks/movimientos/useDeleteMovimiento.jsx
+++ frontend/src/hooks/movimientos/useDeleteMovimiento.jsx
@@ -2,22 +2,52 @@ import { deleteMovimiento } from '@services/movimientos.service.js';
 import Swal from "sweetalert2";
 
 
+async function confirmDeleteMovimientos() {
+  const result = await Swal.fire({
+    title: "¿Estás seguro?",
+    text: "No podrás deshacer esta acción",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Sí, eliminar",
+    cancelButtonText: "Cancelar",
+  });
+  return result.isConfirmed;
+}
+
+async function confirmAlert() {
+  await Swal.fire({
+    title: "Movimiento eliminado",
+    text: "El Movimiento ha sido eliminado correctamente",
+    icon: "success",
+    confirmButtonText: "Aceptar",
+  });
+}
+
+async function confirmError() {
+  await Swal.fire({
+    title: "Error",
+    text: "No se pudo eliminar el movimiento",
+    icon: "error",
+    confirmButtonText: "Aceptar",
+  });
+}
+
+
 export const useDeleteMovimiento = (fetchMovimientos) => {
     const handleDeleteMovimiento = async (movimientoId) => {
         try {
-            const response = await deleteMovimiento(movimientoId);
-            if (response) {
-                Swal.fire({
-                    title: "Movimiento eliminado",
-                    text: "El movimiento ha sido eliminado correctamente",
-                    icon: "success",
-                    confirmButtonText: "Aceptar",
-                });
+            const isConfirmed = await confirmDeleteMovimientos();
+            if (isConfirmed){
+             const response = await deleteMovimiento(movimientoId);
+              if (response) {
+                confirmAlert();
                 await fetchMovimientos();
+              } 
             }
         } catch (error) {
             console.error("Error al eliminar movimiento:", error);
-        }
+            confirmError();
+            }
         };
 
         return { handleDeleteMovimiento }
